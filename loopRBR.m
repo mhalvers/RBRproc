305141693436cb75950c6ecf4e8175315e329bd1
Mark Halverson <mhalvers@eoas.ubc.ca>
2017-03-18 12:32:34 -0700

    Made descent rate an input and acceleration and optional input

--- loopRBR.m
+++ loopRBR.m
@@ -1,39 +1,58 @@
-function out = loopRBR(in,replaceWith)
+function out = loopRBR(in,replaceWith,minDescentRate,minAccelRate)
 %
-% usage: out = loopRBR(in,replaceWith)
+%
+% Filter channels based on the logger descent rate and (optionally)
+% acceleration.
+%
+% usage: out = loopRBR(in,replaceWith,minDescentRate,minAccelRate)
 %
 %
 %   where
-%     in        : structure of rbr data created by output from
-%                 rbrExtractVals.m
+%     in           : Structure of rbr data created by output from
+%                    rbrExtractVals.m
 %
-%    replaceWith : How to treat the flagged values:
-%                :   one of 'interp' or 'NaN'
-%                :     'interp'   - linear interpolation over flagged values
-%                :     'NaN'      - replace flagged values with NaN
+%    replaceWith   : Switch specifying how to treat the flagged values:
+%                  :     'interp'   - linear interpolation over flagged values
+%                  :     'NaN'      - replace flagged values with NaN
+%                
+%    minDescentRate: Threshold descent rate.  Data below this threshold
+%                    are flagged and replaced with a NaN or an 
+%                    interpolated value.
+%
+%    minAccelRate  : (Optional) Threshold acceleration.  Data below this 
+%                    threshold are flagged and replaced with a NaN or an 
+%                    interpolated value.
 %
 %
 % loopRBR identifies scans for which the data is likely of poor
 % quality because of slow descent rates and deceleration.  In such
 % situations, the hydrodynamic wake downstream of the profiler can
 % catch up and distort the measurements.  Slow descent rates or
-% deceleration's are often caused by ship heave, especially when the
-% instrument is being lowered by a winch (i.e., not free falling).
+% decelerations are often caused by ship heave, especially when the
+% instrument is being lowered by a taut wire (i.e., not free falling).
 % 
 % The Hakai RBR descent rate working group found that density
-% inversions typically occurred during periods of slow descent rate and
-% deceleration.  In particular, the data were suspect when the
+% inversions typically occurred during periods of slow descent rate
+% and deceleration.  In particular, the data were suspect when the
 % following criteria were met:
 %
 %  The deceleration falls below -0.1 m/s^2 AND ...
 %  the drop speed falls below 0.4 m/s.
 %
-% Finally, note that these values are derived for the downcast.
-% Applying loopRBR to the upcast will produce strange results, however
-% the upcast is not generally useful for scientific purposes.
+% Finally, note that these values are derived for the downcast of
+% lowered CTD data.  In cases where the upcast is desireable, such as
+% when mounted on a Wirewalker or autonomous Lagrangian float, specify
+% a negative descent rate.
 
-minDescentRate = 0.4; % m/s
-minDecelRate = -0.1;  % m/s^2
+
+%% input checking
+if nargin<3,
+  error(['loopRBR requires at least 3 arguments'])
+end
+if nargin==3,
+  minAccelRate = [];
+end
+
 
 out = in;
 
@@ -46,11 +65,18 @@ end
 %% Calculate descent rate
     
 % first smooth the Depth time series
-np = 3; 
+np = 5; 
 fltr = boxcar(np)/sum(boxcar(np));fltr = fltr(:);
 fdpth = filtfilt(fltr,1,out.Depth);
 
-out.DescentRate = diff(fdpth)./out.samplingPeriod; % m/s
+if isa(out.samplingPeriod,'duration'),
+  dt = seconds(out.samplingPeriod);
+else
+  dt = out.samplingPeriod;
+end
+
+
+out.DescentRate = diff(fdpth)./dt; % m/s
     
 % put the descent rate on the original time stamp
 mtime = out.mtime(1:end-1) + diff(out.mtime)/2;
@@ -60,21 +86,27 @@ out.units(end+1) = {'m/s'};
      
 
 %% Calculate acceleration
-out.DecelRate = diff(out.DescentRate)./out.samplingPeriod; % m/s^2
-mtime = out.mtime(1:end-1) + diff(out.mtime)/2;
-out.DecelRate = interp1(mtime,out.DecelRate,out.mtime,...
-                        'linear','extrap');
-out.units(end+1) = {'m/s^2'};
-
+if ~isempty(minAccelRate),
+  out.AccelRate = diff(out.DescentRate)./dt; % m/s^2
+  mtime = out.mtime(1:end-1) + diff(out.mtime)/2;
+  out.AccelRate = interp1(mtime,out.AccelRate,out.mtime,...
+                          'linear','extrap');
+  out.units(end+1) = {'m/s^2'};
+end
 
 %% flag data which do not meet the minimum velocity and acceleration criteria
-kk = out.DescentRate <= minDescentRate & ...
-     out.DecelRate   <= minDecelRate;
 
-kk = kk | out.DescentRate < 0; % also toss extreme cases when CTD loops
+% velocity
+kk = out.DescentRate <= minDescentRate;
+
+% acceleration
+if ~isempty(minAccelRate),
+  kk = kk & out.AccelRate;
+end
+    
 
 
-%% apply the flag to the sensor data
+%% apply the action to the flagged data
 
 % first develop a list of sensors
 % vars = out.channels;
@@ -93,7 +125,7 @@ vars = vars(~strcmp(vars,{'mtime'}));
 vars = vars(~strcmp(vars,{'Pressure'}));
 vars = vars(~strcmp(vars,{'Depth'}));
 vars = vars(~strcmp(vars,{'DescentRate'}));
-vars = vars(~strcmp(vars,{'DecelRate'}));
+vars = vars(~strcmp(vars,{'AccelRate'}));
 
 
 for j = 1:length(vars),
@@ -117,12 +149,22 @@ end
 
 %% append the processing log
 
-nlog = length(in.processingLog);
+if isfield(in,'processingLog');
+  nlog = length(in.processingLog);
+else
+  nlog = 0;
+end
 
-out.processingLog(nlog+1) = {['Scans for which the descent rate was ' ...
-                              'lower than ' num2str(minDescentRate) ' m/s ' ...
-                              'and the deceleration rate was lower ' ...
-                              'than ' num2str(minDecelRate) ' m/s^2 were ' ...
-                              'replaced with ' replaceWith '.']};
+if ~isempty(minAccelRate),
+  out.processingLog(nlog+1) = {['Scans for which the descent rate was ' ...
+                                'lower than ' num2str(minDescentRate) ' m/s ' ...
+                                'and the deceleration rate was lower ' ...
+                                'than ' num2str(minAccelRate) ' m/s^2 were ' ...
+                                'replaced with ' replaceWith '.']};
+else
+  out.processingLog(nlog+1) = {['Scans for which the descent rate was ' ...
+                                'lower than ' num2str(minDescentRate) ' m/s ' ...
+                                'were replaced with ' replaceWith '.']};
+end
 
 
